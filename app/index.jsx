f37cb1f4734c8be1ed5896ef3ca0b8aa0781fe90
Thomas Santos <thomas@socialeffort.co>
2017-10-02 00:49:18 -0400

    add Notes.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,19 @@
+// import modules
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// import components
+import Notes from './components/Notes';
+
+// set port
 if(process.env.NODE_ENV !== 'production') {
   React.Perf = require('react-addons-perf');
 }
 
 ReactDOM.render(
-  <div>Hello world</div>,
+
+  // React Component Syntax
+  // <(COMPONENT NAME) />
+  <Notes />,
   document.getElementById('app')
 );
