4b46361df7863da98003d2f96553eb1a4435c284
Thomas Santos <thomas@socialeffort.co>
2017-10-02 01:24:34 -0400

    add x button to delete

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
 
 // same as passing (props) => <div>{props.task}</div>
-export default ({task}) => <div>{task}</div>;
+
+// use onDelete callback to begin note deletion process
+export default ({task, onDelete}) => (
+    <div>
+        <span>{task}</span>
+        <button onClick={onDelete}>x</button>
+    </div>
+);
