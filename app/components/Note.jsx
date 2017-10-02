6b9411808b05c4a2cb1564c2f92e2edba77e76f3
Thomas Santos <thomas@socialeffort.co>
2017-10-02 06:27:52 -0400

    dynamically add buttons to Note.jsx

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -3,4 +3,10 @@ import React from 'react';
 // same as passing (props) => <div>{props.task}</div>
 
 // use onDelete callback to begin note deletion process
-export default ({task}) => <div>{task}</div>;
+
+export default ({task, onDelete}) => (
+  <div>
+    <span>{task}</span>
+    <button onClick={onDelete}>x</button>
+  </div>
+);
