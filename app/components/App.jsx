6222d62e041445644bad6dbc23fff0c9b06184ad
Thomas Santos <thomas@socialeffort.co>
2017-10-02 15:28:17 -0400

    refactor app

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 import uuid from 'uuid';
 import Notes from './Notes';
 
-
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+      super(props);
   
       this.state = {
         notes: [
@@ -19,39 +18,37 @@ export default class App extends React.Component {
           }
         ]
       };
-  }
-  render() {
-    const {notes} = this.state;
+    }
+    render() {
+      const {notes} = this.state;
+  
+      return (
+        <div>
+            {/* <button onClick={() => console.log('add note')}>+</button> */}
+            <button onClick={this.addNote}>+</button>
 
-    return (
-      <div>
-        <button onClick={this.addNote}>+</button>
-
-        <Notes notes={notes} onDelete={this.deleteNote} />
-
+        <Notes notes={notes} />
       </div>
     );
   }
   addNote = () => {
-    this.state = {
-    notes: [
-          {
-            id: uuid.v4(),
-            task: 'Learn React'
-          },
-          {
-            id: uuid.v4(),
-            task: 'Do laundry'
-          }
-        ]
-    };
-    }
-  deleteNote = (id, e) => {
-    // Avoid bubbling to edit
-    e.stopPropagation();
-
+    // It would be possible to write this in an imperative style.
+    // I.e., through `this.state.notes.push` and then
+    // `this.setState({notes: this.state.notes})` to commit.
+    //
+    // I tend to favor functional style whenever that makes sense.
+    // Even though it might take more code sometimes, I feel
+    // the benefits (easy to reason about, no side effects)
+    // more than make up for it.
+    //
+    // Libraries, such as Immutable.js, go a notch further.
     this.setState({
-      notes: this.state.notes.filter(note => note.id !== id)
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+      }])
     });
   }
+
 }
+  
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,12 +1,3 @@
 import React from 'react';
 
-// same as passing (props) => <div>{props.task}</div>
-
-// use onDelete callback to begin note deletion process
-
-export default ({task, onDelete}) => (
-  <div>
-    <span>{task}</span>
-    <button onClick={onDelete}>x</button>
-  </div>
-);
+export default ({task}) => <div>{task}</div>;
