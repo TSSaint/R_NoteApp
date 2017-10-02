4681bfd8534df28ba7d884f0cc9db569a7722adf
Thomas Santos <thomas@socialeffort.co>
2017-10-02 01:05:23 -0400

    refactor component Notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
 import Notes from './Notes';
 
+// import module uuid
+import uuid from 'uuid';
+
+// JSON data
+const notes = 
+[
+    {
+        id: uuid.v4(),
+        task: 'Learn React'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Learn Vue'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Re-Learn Ruby'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Re-Learn Ruby on Rails'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Make some JavaScript based apps'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Do laundry'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Go to sleep'
+      }
+];
+
 export default () => (
     <div>
         <button onClick={() => console.log('add note')}>+</button>
-            <Notes />
+            <Notes notes={notes} />
     </div>
 );
