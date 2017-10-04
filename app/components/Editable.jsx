45d41d1f5c1e784ec42b0f13aa29794982af41e2
Thomas Santos <thomas@socialeffort.co>
2017-10-03 23:58:04 -0400

    use package classanmes to turn some input into string

--- app/components/Editable.jsx
+++ app/components/Editable.jsx
@@ -1,23 +1,34 @@
 import React from 'react';
 
-export default ({editing, value, onEdit, ...props}) => {
+import classnames from 'classnames';
+
+export default ({editing, value, onEdit, className, ...props}) => {
   if(editing) {
-    return <Edit value={value} onEdit={onEdit} {...props} />;
-  }
-  return <span {...props}>{value}</span>;
+    return <Edit
+    className={className}
+    value={value}
+    onEdit={onEdit}
+    {...props} />;
+}
+  return <span className={classnames('value', className)} {...props}>
+{value}
+</span>;
+
 }
 class Edit extends React.Component {
   render() {
-    const {value, onEdit, ...props} = this.props;
+    const {className, value, onEdit, ...props} = this.props;
 
-    return <input
-      type="text"
-      autoFocus={true}
-      defaultValue={value}
-      onBlur={this.finishEdit}
-      onKeyPress={this.checkEnter}
-      {...props} />;
-  }
+        return <input
+          type="text"
+    
+          className={classnames('edit', className)}
+          autoFocus={true}
+          defaultValue={value}
+          onBlur={this.finishEdit}
+          onKeyPress={this.checkEnter}
+          {...props} />;
+      }
   checkEnter = (e) => {
     if(e.key === 'Enter') {
       this.finishEdit(e);
