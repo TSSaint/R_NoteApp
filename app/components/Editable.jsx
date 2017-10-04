02d7c1b4ccfe2312cea265f0d5c601676ceae0a6
Thomas Santos <thomas@socialeffort.co>
2017-10-03 23:22:14 -0400

    refactor Editable

--- app/components/Editable.jsx
+++ app/components/Editable.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 
 export default ({editing, value, onEdit, ...props}) => {
-    if(editing) {
-        return <Edit value={value} onEdit={onEdit} {...props} />;
-    }
-}
+  if(editing) {
+    return <Edit value={value} onEdit={onEdit} {...props} />;
+  }
 
-    return <span {...props}>value: {value}</span>;
-
+  return <span {...props}>value: {value}</span>;
 }
 
 const Edit = ({onEdit = () => {}, value, ...props}) => (
-    <div onClick={onEdit} {...props}>
-      <span>edit: {value}</span>
-    </div>
+  <div onClick={onEdit} {...props}>
+    <span>edit: {value}</span>
+  </div>
 );
