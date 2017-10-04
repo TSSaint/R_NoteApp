397a292ef16dc56b2bc09e6521b0bc7acfe4c3c9
Thomas Santos <thomas@socialeffort.co>
2017-10-03 23:32:05 -0400

    refactor App. allow editing of notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -32,10 +32,17 @@ export default class App extends React.Component {
       return (
         <div>
             <button onClick={this.addNote}>+</button>
-            <Notes notes={notes} onDelete={this.deleteNote} />
+            
+            <Notes
+            notes={notes}
+            onNoteClick={this.activateNoteEdit}
+            onEdit={this.editNote}
+            onDelete={this.deleteNote}
+            />
+  
         </div>
-    );
-  }
+      );
+    }
 
   addNote = () => {
       //
@@ -54,6 +61,31 @@ export default class App extends React.Component {
 
     this.setState({
       notes: this.state.notes.filter(note => note.id !== id)
+    });
+  }
+
+  activateNoteEdit = (id) => {
+    this.setState({
+      notes: this.state.notes.map(note => {
+        if(note.id === id) {
+          note.editing = true;
+        }
+
+        return note;
+      })
+    });
+  }
+
+  editNote = (id, task) => {
+    this.setState({
+      notes: this.state.notes.map(note => {
+        if(note.id === id) {
+          note.editing = false;
+          note.task = task;
+        }
+
+        return note;
+      })
     });
   }
 }
